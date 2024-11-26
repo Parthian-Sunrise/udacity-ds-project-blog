716391aca10447627535a144163ffd42a909f5ed
parthian-sunrise <jude.king@purplesmash.com>
2024-11-26 17:09:53 +0000

    feat: questions for project

---
+++ questions/questions.py
@@ -0,0 +1,6 @@
+questions = {
+    "1": "Q1: How much do neighbourhoods affect the price of AirBnbs?",
+    "2": "Q2: 花見 (hanami) is the Japanese practice of watching cherry blossoms bloom, in Japan due to a previous blight among the tree population around 80% of cherry blossoms are genetically identical meaning the vast majority will bloom under the same conditions. Forecasts exist and are well known, for the date of mass blooming. Does the date of blooming seem to impact the price?",
+    "3": "Q3: Which features of the dataset have the best predictive power for the price of a propert based on logistic regression?",
+    "4": "Q4 : What are the features that best predict superhost status?",
+}
